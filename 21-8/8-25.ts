be5e03cd3c2cec0e42ed964194e899ad0d99c4cf
touchpillow <1071721873@qq.com>
2021-08-26 01:00:48 +0800

    add

---
+++ 21-8/8-25.ts
@@ -0,0 +1,44 @@
+namespace N825 {
+  class A {
+    #A = 1;
+    static #AAA() {
+      console.log(111);
+    }
+    A() {
+      console.log(this.#A);
+    }
+  }
+  const a = new A();
+
+  interface AlignValue1 {
+    type: "vertical" | "horizontal";
+    value: "middle" | "center";
+    key: "vertical-align" | "text-align";
+  }
+
+  interface VerticalAlign {
+    type: "vertical";
+    value: "middle";
+    key: "vertical-align";
+  }
+  interface HorizontalAlign {
+    type: "horizontal";
+    value: "center";
+    key: "text-align";
+  }
+
+  type AlignValue2 = VerticalAlign | HorizontalAlign;
+  let align: VerticalAlign;
+  handle("center", "vertical-align");
+  function handle(
+    value: VerticalAlign["value"],
+    key: VerticalAlign["key"]
+  ): void;
+  function handle(
+    value: HorizontalAlign["value"],
+    key: HorizontalAlign["key"]
+  ): void;
+  function handle(v: AlignValue2["value"], key: AlignValue2["key"]) {
+    console.log(v);
+  }
+}
